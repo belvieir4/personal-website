c39787e9b3c1b617249dcc08f84b2e4b23986b4b
Isabel Vieira <work.isabelvieira@gmail.com>
2024-01-16 16:55:36 -0700

    feat: created ebanx's project page

---
+++ src/pages/projects/ebanx/Ebanx.tsx
@@ -0,0 +1,13 @@
+import Hero from 'components/Hero';
+
+const Ebanx = () => {
+  return (
+    <Hero
+      tags={['Product Design', 'Responsive Website']}
+      title="Automated Refund Status Flow"
+      subtitle="A case on how we can improve the user's experience on a Help Center automating the access to information."
+    />
+  );
+};
+
+export default Ebanx;
